ee4f12d1b117b59a004a55769fbcdf570d5e121b
shenchensucc <shenchensucc@gmail.com>
2023-07-07 01:31:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const uri = process.env.ALAS_URI;
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 
