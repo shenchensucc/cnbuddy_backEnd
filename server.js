668b1a3c0be988ed6c863c401c95ad511bce95cc
shenchensucc <shenchensucc@gmail.com>
2023-07-06 23:53:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mogoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = process.env.ALAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const connection = mongoose.connection;
+coonection.once('open', () => {
+    console.log("MongoDB connected");
+});
+
 
 app.use(cors());
 app.use(express.json());
