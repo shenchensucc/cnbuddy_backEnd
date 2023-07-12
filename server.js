a020e66e94e9745696b0fad4fa3f3a13f09ebfcb
shenchensucc <shenchensucc@gmail.com>
2023-07-12 15:29:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+//port is default 5000 for react applciation 
 const port = process.env.PORT || 5000;
-//const uri = process.env.ATLAS_URI;
+
+//Connect to the mongobd in the VPS machine
 mongoose.connect('mongodb://marcoxzh3:MarcoXZh3_ualberta.ca@localhost:27017/steemitdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -18,13 +20,15 @@ mongoose.connect('mongodb://marcoxzh3:MarcoXZh3_ualberta.ca@localhost:27017/stee
     console.error('Error', error);
   });
 
+//use delegatorRouter 
 const readDelegatorRouter = require('readDelegator');
-
 app.use('/cnbuddy-delegator', readDelegatorRouter);
 
+//do not know what below means
 app.use(cors());
 app.use(express.json());
 
+//listening to the port request
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
