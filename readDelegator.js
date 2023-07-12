acc59d9226f0be7aaf356f05e3f3a0e628b2029f
shenchensucc <shenchensucc@gmail.com>
2023-07-12 15:44:30 -0600

    Update readDelegator.js

--- readDelegator.js
+++ readDelegator.js
@@ -2,4 +2,15 @@ const router = require('express').Router();
 const  mongoose = require('mongoose');
 
 
- 
+router.get('/', async (req, res) => {
+  try {
+    const collectionName = 'steemitdb'; // connect with the db
+    const data = await mongoose.connection.db.collection(collectionName).find().toArray();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from the database' });
+  }
+});
+
+
+module.exports = router;
