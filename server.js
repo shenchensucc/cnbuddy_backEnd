ac8c609eb98f2bb506056be719f389d750ab1070
shenchensucc <shenchensucc@gmail.com>
2023-07-13 16:15:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://marcoxzh3:MarcoXZh3_ualberta.ca@localhost:27017/stee
   });
 
 const connection = mongoose.connection;
-conection.once('open', () =>{
+connection.once('open', () =>{
   console.log("MongoDB -once open- tested successfully");
 });
 
