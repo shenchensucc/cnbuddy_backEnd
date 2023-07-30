f81f559a6f02b584881dbb76474edb0cfd7878f9
shenchensucc <shenchensucc@gmail.com>
2023-07-30 00:50:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+// Allow all origins, replace '*' with the specific origin if needed
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 require('dotenv').config();
 
 const app = express();
